7fea98a79d260876db3ffaa9768644dd8ae6fed7
toof-jp <toof.jp.mail@gmail.com>
2022-07-18 00:53:26 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::fmt::Write as _;
 fn generate_diary_template(start_date: &Date<Local>) -> String {
     let mut diary_template = String::new();
     let mut date = Date::clone(start_date);
-    write!(&mut diary_template, "# {}\n", &date.format("%F")).unwrap();
+    writeln!(&mut diary_template, "# {}", &date.format("%F")).unwrap();
     for _ in 0..7 {
-        write!(&mut diary_template, "## {}\n\n", &date.format("%F")).unwrap();
+        writeln!(&mut diary_template, "## {}\n", &date.format("%F")).unwrap();
         date = date + Duration::days(1);
     }
     diary_template

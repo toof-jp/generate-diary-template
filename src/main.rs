2010fd5ee4724e980e266d9faf98707295f81e32
toof-jp <toof.jp.mail@gmail.com>
2022-07-18 00:05:39 +0900

    Add  function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use chrono::prelude::*;
+use chrono::Duration;
+
+fn generate_diary_template(start_date: &Date<Local>) -> String {
+    let mut diary_template = String::new();
+    let mut date = Date::clone(start_date);
+    diary_template.push_str(&format!("# {}\n", &date.format("%F")));
+    for _ in 0..7 {
+        diary_template.push_str(&format!("## {}\n\n", &date.format("%F")));
+        date = date + Duration::days(1);
+    }
+    diary_template
+}
+
 fn main() {
-    println!("Hello, world!");
+    let local_date = Local::today();
+    println!("{}", generate_diary_template(&local_date));
 }

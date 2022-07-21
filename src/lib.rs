739dd227139c23f197b999f5ae12d7d6ddc5dd98
toof-jp <toof.jp.mail@gmail.com>
2022-07-21 22:12:16 +0900

    Split crate

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use chrono::prelude::*;
+use chrono::Duration;
+use std::fmt::Write as _;
+
+pub fn generate_diary_template(start_date: &Date<Local>) -> String {
+    let mut diary_template = String::new();
+    let mut date = Date::clone(start_date);
+    writeln!(&mut diary_template, "# {}", &date.format("%F")).unwrap();
+    for _ in 0..7 {
+        writeln!(&mut diary_template, "## {}\n", &date.format("%F")).unwrap();
+        date = date + Duration::days(1);
+    }
+    diary_template
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use chrono::prelude::*;
-use chrono::Duration;
-use std::fmt::Write as _;
+use generate_diary_template::generate_diary_template;
 
-fn generate_diary_template(start_date: &Date<Local>) -> String {
-    let mut diary_template = String::new();
-    let mut date = Date::clone(start_date);
-    writeln!(&mut diary_template, "# {}", &date.format("%F")).unwrap();
-    for _ in 0..7 {
-        writeln!(&mut diary_template, "## {}\n", &date.format("%F")).unwrap();
-        date = date + Duration::days(1);
-    }
-    diary_template
-}
+use chrono::Local;
 
 fn main() {
     let local_date = Local::today();

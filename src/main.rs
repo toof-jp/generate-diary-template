ab9098760ac291f9a245db31d9857f87b378a425
toof <toof@toof.jp>
2023-08-04 14:40:54 +0900

    refactor: replace `writeln!` with `write!`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use time::OffsetDateTime;
 
 fn main() -> Result<()> {
     let local_date = OffsetDateTime::now_local()?.date();
-    println!("{}", generate_diary_template(&local_date)?);
+    print!("{}", generate_diary_template(&local_date)?);
 
     Ok(())
 }
